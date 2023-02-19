c3eb994ed4d27b695b9bb15c76689cf98daee352
Timy Stream <timy@phldev.net>
2023-02-19 20:53:01 +0100

    Finished Installation Script

--- installer.sh
+++ installer.sh
@@ -23,20 +23,32 @@ echo "Thank you for using this script to install your server."
 echo ""
 echo "What is the Direct Link to the Server Jar File? e.g.: https://example.com/downlaod/server.jar"
 read -p "Download Link: " dwLink
-
 echo ""
 echo "How do you want the 'Tmux Session'/'MC Server' to be named? : "
 read -p "Name: " svName
+echo ""
+echo "How much RAM is your Server allowed to use? e.g.: '6' => 6GB RAM"
+read -p "RAM: " svRAM
 echo ""
 echo "========================================================================================================================================================="
 echo ""
 echo "  ╔═Your Answers"
 echo "  ║Download Link:   "$dwLink
 echo "  ║Server Name:     "$svName
+echo "  ║RAM:             "$svRAM
 echo "  ╚══════════════════"
 echo ""
 echo "========================================================================================================================================================="
+echo ""
+echo ""
+echo "Downloading Files..."
+curl -sO $dwLink
 
-echo ""
-echo ""
-echo "Starting to Install Dependencies: curl, tmux"
+Serverfile=$(basename $dwLink)
+
+echo 'tmux new-session -S "'$svName'" \; split-window -h \; send-keys "./mcserv.sh" C-m' >> startServer.sh
+chmod +x startServer.sh
+echo 'java -Xmx'$svRAM'G -jar '$Serverfile' nogui' >> mcserv.sh
+chmod +x mcserv.sh
+
+echo "Finished Installation! To start the Server execute 'startServer.sh' you can do this like this: ./startServer.sh"
